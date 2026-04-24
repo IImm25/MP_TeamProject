47b91d67b93c3b0435ca92658d5e1601bfebf44c
Irrgh <schellenberger.erik@gmail.com>
2026-04-24 12:49:39 +0200

    cleanup

--- Backend/Data/Repositories/IRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Backend.Data.Repositories;
-public interface IRepository<T> where T : class
-{
-    Task<List<T>> GetAllAsync();
-    Task<T?> GetByIdAsync(int id);
-    Task AddAsync(T entity);
-    void Update(T entity);
-    void Remove(T entity);
-}
