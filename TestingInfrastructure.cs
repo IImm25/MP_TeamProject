a51d8ab29ae6b38dfcc8300d7be96d1d24f1af0b
Implomin <mail.jarek.nowak@gmail.com>
2026-04-30 04:24:17 +0200

    wip chained system tests

---
+++ TestingInfrastructure.cs
@@ -0,0 +1,12 @@
+﻿[TestClass]
+public class TestingInfrastructure
+{
+    public static WindPowerFactory Factory = null!;
+
+    [AssemblyInitialize]
+    public static async Task AssemblyInit(TestContext context)
+    {
+        Factory = new WindPowerFactory();
+        await Factory.CreateClient().GetAsync("/");
+    }
+}
