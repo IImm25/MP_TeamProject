df6f64f10426cc6f8e96c7f64f691ac9113fed0d
DarkWood1558 <maurice.dd@icloud.com>
2026-06-08 22:18:17 +0200

    added real life turbine coordinates data as default

--- Backend/Data/Entitites/Turbine.cs
+++ Backend/Data/Entitites/Turbine.cs
@@ -6,7 +6,7 @@
         public string Name { get; set; }
         public float Latitude { get; set; }
         public float Longitude { get; set; }
-
+        public Turbine() { }
         public Turbine(string name, float latitude, float longitude)
         {
             Name = name;
